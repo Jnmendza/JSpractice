f1d627f861cb7ee207f09756d721d1a1c552e9f4
Jnmendza <jonatmendoza@gmail.com>
2020-07-19 22:52:42 -0700

    message

---
+++ Message/script.js
@@ -0,0 +1,16 @@
+const sendBtn = document.querySelector('#sendBtn');
+const messageIn = document.querySelector('#messageIn')
+const messageOut = document.querySelector('#messageOut')
+
+
+const sendMsg = () => {
+    let content = messageIn.value;
+    if(content === ''){
+        alert('Please Enter Valid Value')
+    } else {
+        messageOut.innerHTML = content;
+        messageIn.value = '';
+    }
+}
+
+sendBtn.addEventListener('click', sendMsg)
